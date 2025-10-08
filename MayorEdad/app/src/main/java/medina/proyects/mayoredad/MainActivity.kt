7b7479ccc6cee8b4e1f0bb4f058f3db038ebf7de
Cynthia <cynthia@max12>
2025-10-08 13:03:53 +0200

    MayorEdad/

--- MayorEdad/app/src/main/java/medina/proyects/mayoredad/MainActivity.kt
+++ MayorEdad/app/src/main/java/medina/proyects/mayoredad/MainActivity.kt
@@ -27,23 +27,41 @@ private lateinit var binding: ActivityMainBinding
         val num: EditText = findViewById<EditText>(R.id.editTextText2)
         val boton: Button = findViewById<Button>(R.id.button)
         val texto: TextView = findViewById<TextView>(R.id.textView)
+        val botonRestar : Button = findViewById<Button>(R.id.botonRestar)
+        val botonSumar : Button = findViewById<Button>(R.id.botonSumar)
+
+        fun actualizarMensaje(edad : Int?, textoResultado : TextView){
+            val mensaje = when{
+                edad == null -> "Introduzca un valor"
+                edad < 18 -> "Eres menor de edad"
+                edad > 18 -> "Eres mayor de edad"
+                else -> "Tienes justo 18 años"
+            }
+            textoResultado.text = mensaje
+        }
         boton.setOnClickListener {
             //recoger el valor del campo del texto, pasarlo a entero o null
             //poner un if, pero hau que distinguir si es nulo o no
             val numeroResultado = num.text.toString().toIntOrNull()
-            if (numeroResultado == null) {
-                texto.text = "Introduzca un valor"
-            } else if (numeroResultado != null) {
-                if(numeroResultado<18){
-                texto.text = "Eres menor de edad"
-            } else if (numeroResultado > 18)
-                texto.text = "Eres mayor de edad"
-            else {
-                texto.text = "Tienes justo 18 años!!"
+           actualizarMensaje(numeroResultado, texto)
+
+        }
+            botonSumar.setOnClickListener {
+                var numeroSeleccionado = num.text.toString().toInt()
+                numeroSeleccionado++
+                num.setText(numeroSeleccionado.toString())
+                actualizarMensaje(numeroSeleccionado, texto)
             }
-        }
+            botonRestar.setOnClickListener {
+                var numeroSeleccionado= num.text.toString().toInt()
+                numeroSeleccionado--
+                num.setText(numeroSeleccionado.toString())
+                actualizarMensaje(numeroSeleccionado, texto)
+            }
+
 
     }
 
+
         }
-    }
+
