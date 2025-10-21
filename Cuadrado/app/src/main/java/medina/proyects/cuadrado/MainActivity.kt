3874683506879360d5b62b58d224e2d8f067def8
Cynthia <cynthia@max12>
2025-10-21 13:17:24 +0200

    Guardado

---
+++ Cuadrado/app/src/main/java/medina/proyects/cuadrado/MainActivity.kt
@@ -0,0 +1,56 @@
+package medina.proyects.cuadrado
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+import androidx.core.view.WindowInsetsCompat
+import model.Cuadrado
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //identificacion del cuadrado vista
+        val cuadradoView : View = findViewById(R.id.cuadrado)
+        //asociar a la vista con el objeto cuadrado
+        //ContextCompat es una clase para acceder a recursos
+        val cuadrado : Cuadrado = Cuadrado(ContextCompat.getColor(this, R.color.red), 100,100)
+
+
+        //identifiacacion de botones
+
+        val botonArriba : Button = findViewById<Button>(R.id.buttonArriba)
+        val botonAbajo : Button = findViewById<Button>(R.id.buttonAbajo)
+        val botonIzquierda : Button = findViewById<Button>(R.id.buttonIzquierda)
+        val botonDerecha : Button = findViewById<Button>(R.id.buttonDerecha)
+        val botonCambiarColor : Button = findViewById<Button>(R.id.buttonCambiarColor)
+        val botonCambiarTamanio : Button = findViewById<Button>(R.id.buttonCambiarTamanio)
+
+        //ponemos botones a la escucha
+        botonArriba.setOnClickListener {
+            cuadrado.moverArriba()
+        }
+        botonAbajo.setOnClickListener {
+            cuadrado.moverAbajo()
+        }
+        botonDerecha.setOnClickListener {
+            cuadrado.moverDerecha()
+        }
+        botonIzquierda.setOnClickListener {
+            cuadrado.moverIzquierda()
+        }
+        botonCambiarTamanio.setOnClickListener {
+            cuadrado.cambiarTamanio(10,10)
+        }
+        botonCambiarColor.setOnClickListener {
+
+        }
+
+
+    }
+}
--- Cuadrado/app/src/main/java/model/Cuadrado.kt
+++ Cuadrado/app/src/main/java/model/Cuadrado.kt
@@ -1,4 +1,31 @@
 package model
 
-class Cuadrado {
+class Cuadrado (var color: Int , var ancho: Int, var alto : Int ) {
+    // Coordenadas iniciales
+    var x : Int = 0
+    var y : Int = 0
+
+    //métodos para mover-cambiar el cuadrado
+    fun moverArriba(){
+        y-= 10 //y = y - 10
+
+    }
+    fun moverAbajo(){
+        y+=10; //
+    }
+    fun moverDerecha(){
+        x+=10;
+    }
+    fun moverIzquierda(){
+        x-=10;
+    }
+    fun cambiarTamanio(nuevoAncho: Int, nuevoAlto : Int){
+        alto = nuevoAlto
+        ancho = nuevoAncho
+    }
+
+
+
+
+
 }
