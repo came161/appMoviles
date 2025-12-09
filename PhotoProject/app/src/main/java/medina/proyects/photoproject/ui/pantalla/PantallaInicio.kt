fd9bdc518262bcb773eb54510f4c1b6a4dfc0a4e
cynthia <cynthia16123@gmail.com>
2025-12-09 13:44:29 +0100

    EJERCICIOS

---
+++ PhotoProject/app/src/main/java/medina/proyects/photoproject/ui/pantalla/PantallaInicio.kt
@@ -0,0 +1,42 @@
+package medina.proyects.photoproject.ui.pantalla
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun PantallaInicio(navController: NavController){
+    Column (
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text("Bienvenido a la galeria")
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {}) {
+            Text("Ir a la galeria")
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {}) {
+            Text("Informacion de la app")
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+
+}
+/*
+@Preview(showBackground = true)
+@Composable
+fun PantallaInicio(){
+    PantallaInicio(navController = NavController)
+}
+*/
